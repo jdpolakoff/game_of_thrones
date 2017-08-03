f1da3455140c59e31bcb018b5a58dcb0282fa21c
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-03 11:20:12 -0400

    full houses CRUD

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,6 +6,32 @@ class HousesController < ApplicationController
 
   def show
     @house = House.find(params[:id])
-  end 
+  end
+
+  def new
+    @house = House.new
+  end
+
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to @house
+  end
+
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to @house
+  end
+
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :img_url, :motto)
+  end
 
 end
