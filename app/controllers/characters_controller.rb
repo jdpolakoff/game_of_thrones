bd0f99814b1e6c418381c9e255921e72577f158d
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-03 10:55:56 -0400

    full CRUD for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,5 +9,39 @@ class CharactersController < ApplicationController
     @character = Character.find(params[:id])
   end
 
+  def new
+    @house = House.find(params[:house_id])
+    @character = @house.characters.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create!(character_params)
+    redirect_to @house
+  end
+
+  def edit
+    @house = House.find(params[:house_id])
+    @character = @house.characters.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:house_id])
+    @character = @house.characters.find(params[:id]).update(character_params)
+    redirect_to @house
+  end
+
+  def destroy
+    @house = House.find(params[:house_id])
+    @character = @house.characters.find(params[:id]).destroy
+    redirect_to @house
+  end
+
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :img_url)
+  end
+
 
 end
